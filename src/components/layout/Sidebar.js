5cefabafe6abe03a0628009a54e5425965b5bb00
MikiWojak <mikizarowa8@gmail.com>
2023-07-25 12:53:30 +0200

    Show note title

--- src/components/layout/Sidebar.js
+++ src/components/layout/Sidebar.js
@@ -1,19 +1,23 @@
 import React from "react"
 
 export default function Sidebar(props) {
-    const noteElements = props.notes.map((note, index) => (
-        <div key={note.id}>
-            <div
-                
-                className={`title ${
-                    note.id === props.currentNote.id ? "selected-note" : ""
-                }`}
-                onClick={() => props.setCurrentNoteId(note.id)}
-            >
-                <h4 className="text-snippet">Note {index + 1}</h4>
+    const noteElements = props.notes.map((note, index) => {
+        const lines = JSON.stringify(note.body).split('\\n')
+        const [rawTitle] = lines
+        const title = rawTitle.replace(/[^a-zA-Z0-9 ]/g, '').trim()
+
+        return (
+            <div key={note.id}>
+                <div
+                    className={`title ${
+                        note.id === props.currentNote.id ? "selected-note" : ""
+                    }`}
+                    onClick={() => props.setCurrentNoteId(note.id)}
+                >
+                    <h4 className="text-snippet">{title}</h4>
+                </div>
             </div>
-        </div>
-    ))
+    )})
 
     return (
         <section className="pane sidebar">
